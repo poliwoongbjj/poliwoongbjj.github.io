6e78e124954c89eaba7ffe807e3a51fc1f9d1360
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-02-13 02:36:19 +0900

    Create Hero Component and add to Home Page

---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      {/* Add more sections below the hero as needed */}
+    </div>
+  );
+};
+
+export default Home;
